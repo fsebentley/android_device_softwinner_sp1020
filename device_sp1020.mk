6c7711851f051318de6d67b2f5d89d97eb34cc01
Kevin <dumbandroid@gmail.com>
2014-02-22 17:26:44 -0800

    move fstab.sp1020 to root

--- device_sp1020.mk
+++ device_sp1020.mk
@@ -41,7 +41,8 @@ PRODUCT_COPY_FILES += \
     device/softwinner/sp1020/init.sp1020.usb.rc:root/init.sp1020.usb.rc \
     device/softwinner/sp1020/recovery/init_parttion.sh:root/sbin/init_parttion.sh \
     device/softwinner/sp1020/prebuilt/bin/busybox:root/sbin/busybox \
-    device/softwinner/sp1020/prebuilt/modules/nand.ko:root/nand.ko
+    device/softwinner/sp1020/prebuilt/modules/nand.ko:root/nand.ko \
+    device/softwinner/sp1020/fstab.sp1020:root/fstab.sp1020
 
 # Modules for recovery
 PRODUCT_COPY_FILES += \
